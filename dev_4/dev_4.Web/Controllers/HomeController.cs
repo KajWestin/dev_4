0490300d8b9aea8ab42319ad46b150663f7bce1c
KajWestin <kaj.westin@live.se>
2016-09-25 12:51:50 +0200

    Fick ordning på referenserna nu

--- dev_4/dev_4.Web/Controllers/HomeController.cs
+++ dev_4/dev_4.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dev_4.Domain.Logic;
 
 namespace dev_4.Web.Controllers
 {
@@ -10,6 +11,25 @@ namespace dev_4.Web.Controllers
     {
         public ActionResult Index()
         {
+
+            var veh = new Vehicle();
+
+            var firstS = veh.GetSpeed();
+
+            veh.IncreaseSpeed(10);
+
+            var secS = veh.GetSpeed();
+
+            veh.IncreaseSpeed(100);
+
+            var thS = veh.GetSpeed();
+
+            veh.DecreaseSpeed(40);
+
+            var lastS = veh.GetSpeed();
+
+
+
             return View();
         }
 
